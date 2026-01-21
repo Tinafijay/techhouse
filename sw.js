7fa13fd95588994aab9e290d0e1cfd3872e35f64
Tinafijay <Tinafijonathan@gmail.com>
2026-01-21 12:01:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = 'tech-house-v1';
+const CACHE_NAME = 'tech-house-v3'; // Incremented version to force update
 const ASSETS = [
+    './', // Cache the root
     './poisoning%20detector.html',
     './poisoning%20detector%20styles.css',
     './poisoning%20detector%20script.js',
     './manifest.json',
     './Tech%20House%20logo.jpg',
-    './vista_startup.mp3' // Caching the completion sound
+    './vista_startup.mp3'
 ];
 
 // Install: Cache all branding and audio files
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force the new service worker to take over immediately
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS);
@@ -17,7 +19,7 @@ self.addEventListener('install', (event) => {
     );
 });
 
-// Activation: Clean up old versions
+// Activation: Clean up old versions to save space
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((keys) => {
